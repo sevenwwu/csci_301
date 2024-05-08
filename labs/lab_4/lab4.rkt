b8705aa941b18e5ea14df3ec1a01ae388e49f0a0
sevenwwu <lewisi4@wwu.edu>
2024-05-08 13:05:49 -0700

    removed helper methods for elegance added comments for clarity

--- labs/lab_4/lab4.rkt
+++ labs/lab_4/lab4.rkt
@@ -14,12 +14,6 @@
     )
 )
 
-;;; (member? 1 '(3 2 1)) ;; ;; ---> #t
-;;; (member? 4 '(3 2 1)) ;; ;; ---> #f
-;;; (member? 1 '());; ;; ---> #f
-;;; (member? 'susan '(susan john ryan)) ;; ;; ---> #t
- 
-
 
 (define (subset? s1 s2)
     (cond 
@@ -49,63 +43,50 @@
 ;;; (set-equal? '(1 2) '(3 2 1)) ;; ;; ---> #f
 ;;; (set-equal? '(ryan susan john) '(susan john ryan)) ;; ;; ---> #t
 
-(define (remove-duplicates s)
-    (cond
-        ([null? s]
+
+;;; union checks to see if it's our first run (when s2 is not null)
+;;; if so, throw everything into s1, set s2 to null and run it again.
+;;; Then, we simply remove duplicates for s1 until s1 is empty, and 
+;;; join all the unique elements together. 
+(define (union s1 s2)
+    (cond 
+        ([not (null? s2)] 
+            (union (append s1 s2) '())
+        )
+        ([null? s1]
             '()
         )
-        ([member? (car s) (cdr s)]
-            (remove-duplicates (cdr s))
-        )
-        (else
-            (cons (car s) (remove-duplicates (cdr s)))
-        )
-    )
-)
-
-(define (union s1 s2)
-    (union-helper (remove-duplicates s1) (remove-duplicates s2))
-)
-
-(define (union-helper s1 s2)
-    (cond 
-        ([null? s2] 
-            s1
-        )
-        ([member? (car s2) s1] 
-            (union-helper s1 (cdr s2))
+        ([member? (car s1) (cdr s1)] 
+            (union (cdr s1) '())
         )
         (else 
-            (union-helper (append s1 (list (car s2))) (cdr s2))
+            (cons (car s1) (union (cdr s1) '()))
         )
     )
 )
 
-(define (intersect s1 s2)
-    (intersect-helper (remove-duplicates s1) (remove-duplicates s2))
-)
 
-(define (intersect-helper s1 s2)
+;;; intersect checks to see if the first element of s1 is 
+;;; in the rest of s1 or not in s2. If either is the case
+;;; do not add it to the return list. Otherwise, the element
+;;; is unique and should be included in the return list.
+;;; This way shared elements only enter the return list once.
+(define (intersect s1 s2)
     (cond 
         ([null? s1] 
             '()
         )
+        ([member? (car s1) (cdr s1)]
+            (intersect (cdr s1) s2)
+        )
         ([member? (car s1) s2]
-            (cons (car s1) (intersect-helper (cdr s1) s2))
+            (cons (car s1) (intersect (cdr s1) s2))
         )
         (else
-            (intersect-helper (cdr s1) s2)
+            (intersect (cdr s1) s2)
         )
     )
 )
-
-;;; (union '(1 2 3) '(3 2 1)) ;; ;; ---> (1 2 3)
-;;; (union '(1 2 3) '(3 4 5)) ;; ;; ---> (1 2 3 4 5)
-;;; (union '(a b c) '(3 2 1)) ;; ;; ---> (a b c 1 2 3)
-;;; (intersect '(1 2 3) '(3 2 1)) ;; ;; ---> (1 2 3)
-;;; (intersect '(1 2 3) '(4 5 6)) ;; ;; ---> ()
-;;; (intersect '(1 2 3) '(2 3 4 5 6)) ;; ;; ---> (2 3)
-
 
 (union '(1 2 3) '(3 2 2 1)) ;; ---> (1 2 3)
 (union '(1 2 3 2) '(3 4 5)) ;; ---> (1 2 3 4 5)
