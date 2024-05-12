0a829f1eda52c851f632b2c735338650bb8064ea
sevenwwu <lewisi4@wwu.edu>
2024-05-12 15:01:38 -0700

    removed debugging and added comments

--- labs/lab_5/lab5.rkt
+++ labs/lab_5/lab5.rkt
@@ -16,27 +16,34 @@
     )
 )
 
-;;; New stuff for this lab
+;;; Newly Defined Procedures
 
+
+;;; Reflexive? and Reflexive?-helper iterate through each
+;;; relation and check to make sure that:
+;;;    1) If the relationship is of the form (a,a), then
+;;;       make sure that a is in set, and make note that
+;;;       we have now seen the reflexivity of element a
+;;;       (by removing it from the working set)
+;;;    2) If the relationship is of any other form, 
+;;;       just make sure each element is from the given
+;;        set and move on to the next relation.
+;;; Once we make it through all the relations (or return
+;;; an early #f because we found an element not in the
+;;; given set), if we found all reflexive pairs (this
+;;; is when the working set is empty) return #t otherwise
+;;; return false.
 (define (Reflexive? relations set)
     (Reflexive?-helper relations set set)
 )
 
 (define (Reflexive?-helper relations workingSet set)
-    (begin
-        (display "--------------\n")
-        (display relations)
-        (display "\n")
-        (display workingSet)
-        (display "\n")
-        (display set)
-        (display "\n\n\n")
     (cond
         ([and (null? workingSet) (null? relations)] 
-                #t
+            #t
         )
         ((null? relations)
-                #f    
+            #f    
         )
         (else
             (let ([e1 (car (car relations))] [e2 (car (cdr (car relations)))])
@@ -53,7 +60,7 @@
                 )
             )
         )
-    ))
+    )
 )
 
 ;;; (display "Reflexive?\n") 
@@ -62,12 +69,9 @@
 ;;; (Reflexive? '((a a) (a s) (b b) (c c)) '(a b c)) ;; -->#f 
 ;;; (Reflexive? '() '()) ;; -->#t 
 
-
+;;; Symmetric? iterates over each relation (a,b) and checks to 
+;;; make sure that (b,a) is also in the the relations set. 
 (define (Symmetric? relations)
-    (begin
-        (display "--------------\n")
-        (display relations)
-        (display "\n")
     (cond
         ([null? relations]
             #t
@@ -78,7 +82,7 @@
         (else
             #f
         )
-    ))
+    )
 )
 
 ;;; (display "Symmetric?\n") 
@@ -87,16 +91,16 @@
 ;;; (Symmetric? '((a a) (b b)))  ;; --> #t 
 ;;; (Symmetric? '())  ;; --> #t 
 
-
+;;; Transitive?, Transitive?-helper, and TransitiveChecker
+;;; all work together to check all pairs of relations.
+;;; If we ever find that (a b) and (b c), but not (a c),
+;;; we return #f. If this is not found after checking 
+;;; every case of (a b) and (b c), we return #t. 
 (define (Transitive? relations)
     (Transitive?-helper relations relations)
 )
 
 (define (Transitive?-helper iRelations relations)
-    ;;; (begin
-    ;;;     (display "--------------\n")
-    ;;;     (display relations)
-    ;;;     (display "\n")
     (cond
         ([null? iRelations]
             #t
@@ -122,10 +126,10 @@
     )
 )
 
-(display "Transitive? \n")  
-(Transitive? '((a b) (b c) (a c)))  ;; --> #t 
-(Transitive? '((a a) (b b) (c c)))  ;; --> #t 
-(Transitive? '((a b) (b a)))  ;; --> #f 
-(Transitive? '((a b) (b a) (a a)))  ;; --> #f 
-(Transitive? '((a b) (b a) (a a) (b b)))  ;; --> #t 
-(Transitive? '()) ;; --> #t
+;;; (display "Transitive? \n")  
+;;; (Transitive? '((a b) (b c) (a c)))  ;; --> #t 
+;;; (Transitive? '((a a) (b b) (c c)))  ;; --> #t 
+;;; (Transitive? '((a b) (b a)))  ;; --> #f 
+;;; (Transitive? '((a b) (b a) (a a)))  ;; --> #f 
+;;; (Transitive? '((a b) (b a) (a a) (b b)))  ;; --> #t 
+;;; (Transitive? '()) ;; --> #t
