fa7153f37ff801f8d403f611d5b2b061811b6665
sevenwwu <lewisi4@wwu.edu>
2024-05-21 21:09:04 -0700

    changed things to immutable, attempt on nondeterministic finite automa

--- c#/finite_automata/FiniteAutomata.cs
+++ c#/finite_automata/FiniteAutomata.cs
@@ -1,43 +1,54 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 
 
 
 public class FiniteAutomaton<T>
 {
-    private HashSet<T> _states;
-    private HashSet<char> _alphabet;
-    private Dictionary<(T, char), T> _transitionFunction;
-    private T _startState;
-    private HashSet<T> _acceptStates;
+    public readonly ImmutableHashSet<T> States;
+    public readonly ImmutableHashSet<char> Alphabet;
+    public readonly ImmutableDictionary<(T, char), T> TransitionFunction;
+    public readonly T StartState;
+    public readonly ImmutableHashSet<T> AcceptStates;
 
     public FiniteAutomaton(HashSet<T> states, HashSet<char> alphabet, Dictionary<(T, char), T> transitionFunction,
                             T startState, HashSet<T> acceptStates)
     {
-        _states = states;
-        _alphabet = alphabet;
-        _transitionFunction = transitionFunction;
-        _startState = startState;
-        _acceptStates = acceptStates;
+        States = states.ToImmutableHashSet();
+        Alphabet = alphabet.ToImmutableHashSet();
+        TransitionFunction = transitionFunction.ToImmutableDictionary();
+        StartState = startState;
+        AcceptStates = acceptStates.ToImmutableHashSet();
 
         ValidateStartState();
         ValidateAcceptStates();
         ValidateTransitionFunction();
     }
 
+    // Nondeterministic Finite Automaton
+    public static FiniteAutomaton<HashSet<T>> NonDeterministicFiniteAutomaton
+    (HashSet<T> states, HashSet<char> alphabet, Dictionary<(T, char), ImmutableHashSet<T>> transitionFunction,
+                            T startState, HashSet<T> acceptStates)
+    {
+        var States = new HashSet<HashSet<T>>();
+
+        foreach 
+    }
+
     private void ValidateStartState()
     {
-        if (!_states.Contains(_startState))
+        if (!States.Contains(StartState))
         {
-            throw new InvalidOperationException($"Start state {_startState} is not in defined states.");
+            throw new InvalidOperationException($"Start state {StartState} is not in defined states.");
         }
     }
 
     private void ValidateAcceptStates()
     {
-        foreach (var curState in _acceptStates)
+        foreach (var curState in AcceptStates)
         {
-            if (!_states.Contains(curState))
+            if (!States.Contains(curState))
             {
                 throw new InvalidOperationException($"Accept state {curState} is not in defined states.");
             }
@@ -46,11 +57,11 @@ public class FiniteAutomaton<T>
 
     private void ValidateTransitionFunction()
     {
-        foreach (var state in _states)
+        foreach (var state in States)
         {
-            foreach (var symbol in _alphabet)
+            foreach (var symbol in Alphabet)
             {
-                if (!_transitionFunction.ContainsKey((state, symbol)))
+                if (!TransitionFunction.ContainsKey((state, symbol)))
                 {
                     throw new InvalidOperationException($"Transition function is not defined for state {state} and symbol '{symbol}'.");
                 }
@@ -60,19 +71,19 @@ public class FiniteAutomaton<T>
 
     public bool Accepts(string input)
     {
-        T currentState = _startState;
+        T currentState = StartState;
 
         foreach (char symbol in input)
         {
-            if (!_alphabet.Contains(symbol))
+            if (!Alphabet.Contains(symbol))
             {
                 throw new ArgumentException($"Symbol '{symbol}' is not in the language alphabet.");
             }
 
-            currentState = _transitionFunction[(currentState, symbol)];
+            currentState = TransitionFunction[(currentState, symbol)];
         }
 
-        return _acceptStates.Contains(currentState);
+        return AcceptStates.Contains(currentState);
     }
 
 
@@ -80,48 +91,48 @@ public class FiniteAutomaton<T>
     {
         var states = new HashSet<(T1, T2)>();
 
-        foreach (var stateM1 in m1._states)
+        foreach (var stateM1 in m1.States)
         {
-            foreach (var stateM2 in m2._states)
+            foreach (var stateM2 in m2.States)
             {
                 states.Add((stateM1,stateM2));
             }
         }
 
 
-        if (!m1._alphabet.SetEquals(m2._alphabet))
+        if (!m1.Alphabet.SetEquals(m2.Alphabet))
         {
             throw new InvalidOperationException($"Alphabets do not match.");
         }
 
-        var alphabet = new HashSet<char>(m1._alphabet);
+        var alphabet = new HashSet<char>(m1.Alphabet);
 
 
         var transitionFunction = new Dictionary<((T1,T2),char),(T1,T2)>();
-        foreach (var stateM1 in m1._states)
+        foreach (var stateM1 in m1.States)
         {
-            foreach (var stateM2 in m2._states)
+            foreach (var stateM2 in m2.States)
             {
                 foreach (var chr in alphabet)
                 {
-                    transitionFunction.Add(((stateM1,stateM2),chr),(m1._transitionFunction[(stateM1,chr)],m2._transitionFunction[(stateM2,chr)]));
+                    transitionFunction.Add(((stateM1,stateM2),chr),(m1.TransitionFunction[(stateM1,chr)],m2.TransitionFunction[(stateM2,chr)]));
                 }
             }
         }
 
-        var startState = (m1._startState,m2._startState);
+        var startState = (m1.StartState,m2.StartState);
 
         var acceptStates = new HashSet<(T1,T2)>();
-        foreach (var accStateM1 in m1._acceptStates)
+        foreach (var accStateM1 in m1.AcceptStates)
         {
-            foreach (var stateM2 in m2._states)
+            foreach (var stateM2 in m2.States)
             {
                 acceptStates.Add((accStateM1,stateM2));
             }
         } 
-        foreach (var stateM1 in m1._states)
+        foreach (var stateM1 in m1.States)
         {
-            foreach (var accStateM2 in m2._acceptStates)
+            foreach (var accStateM2 in m2.AcceptStates)
             {
                 acceptStates.Add((stateM1,accStateM2));
             }
